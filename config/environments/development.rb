b8ebe990fa5eb9103855956215f8def6482df228
sakamoto15 <155605617+sakamomo15@users.noreply.github.com>
2024-02-25 11:18:11 +0000

    [Fix] controlller_syntaxerror

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,7 @@ Rails.application.configure do
   config.hosts << "49e69874be6d4ef3a0718e9b15430b98.vfs.cloud9.us-east-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "2ffe6807ce4649ad83d36ac2fa76502c.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
 end
