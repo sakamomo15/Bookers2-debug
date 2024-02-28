dad5674c7eb20b00e4ba8a77f9f602cae462c1ed
sakamoto15 <155605617+sakamomo15@users.noreply.github.com>
2024-02-28 07:59:09 +0000

    [Fix] follow_error

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followings, through: :active_relationships, source: :followed
-  has_many :follower, through: :passive_relationships, source: :follower
+  has_many :followers, through: :passive_relationships, source: :follower
 
   has_one_attached :profile_image
 
