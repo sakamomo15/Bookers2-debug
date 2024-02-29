5e5280f5c4286008037f13e0be4df85de314c475
sakamoto15 <155605617+sakamomo15@users.noreply.github.com>
2024-02-29 06:02:45 +0000

    [Add] search

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users # 最初に記載
 
-  root :to => "homes#top"
+  root to: "homes#top"
   get "home/about" => "homes#about"
+  get "/search", to: "searches#search"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorite, only: [:create, :destroy]
